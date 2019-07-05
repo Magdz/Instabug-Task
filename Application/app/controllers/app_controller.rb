269a433e047c5d743e7783c1fd811a5909292bd8
Magdz <magdz_008@yahoo.com>
2019-07-05 23:57:15 +0200

    Add Dockerfile

--- Application/app/controllers/app_controller.rb
+++ Application/app/controllers/app_controller.rb
@@ -4,7 +4,10 @@ class AppController < ApplicationController
         o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
         string = (0...50).map { o[rand(o.length)] }.join
         @app.token = string
+
         if @app.save
+            ActiveRecord::Base.connection.execute("CREATE DATABASE app_" + @app.id.to_s)
+
             render json: @app, status: :ok
         end
     end
--- Application/config/routes.rb
+++ Application/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post 'application', to: 'app#create'
+  post 'applications', to: 'app#create'
 end
