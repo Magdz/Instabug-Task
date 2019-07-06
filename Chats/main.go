51b50c3ccc2375c63c2c49493b598e2b04c01c13
Magdz <magdz_008@yahoo.com>
2019-07-06 03:03:51 +0200

    Publish application token for chat creation to queue

--- Chats/main.go
+++ Chats/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"github.com/adjust/rmq"
 )
 
 type Chat struct {
@@ -15,11 +16,22 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "HomePage endpoint hit!")
 }
 
+func createChat(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	token := vars["token"]
+
+	connection := rmq.OpenConnection("worker", "tcp", "localhost:6379", 1)
+	queue := connection.OpenQueue("chats")
+
+	queue.Publish(token)
+}
+
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", homePage).Methods("GET")
-	
+	router.HandleFunc("/applications/{token}/chats", createChat).Methods("POST")
+
 	log.Fatal(http.ListenAndServe(":8081", router))
 }
 
